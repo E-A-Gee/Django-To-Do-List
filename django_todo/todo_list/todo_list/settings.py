be3e0c75f41fd0a595b2cde24c725ba4895d237a
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-07-03 21:33:10 -0400

    updated settings

--- django_todo/todo_list/todo_list/settings.py
+++ django_todo/todo_list/todo_list/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uw8j5pc$8eyodg3_*#t)lez@#t&=pexq-+r)s&ec#z*sxgt)3p'
+#SECRET_KEY = 'django-insecure-uw8j5pc$8eyodg3_*#t)lez@#t&=pexq-+r)s&ec#z*sxgt)3p'
+SECRET_KEY = os.environ.get('TODO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('TODO_DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['geeliztodo.herokuapp.com']
 
 
 # Application definition
