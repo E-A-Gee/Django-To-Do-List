0c0b2fcf3907a9343fc1988af32852bf59ea028a
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-07-03 22:16:20 -0400

    set debug value

--- django_todo/todo_list/todo_list/settings.py
+++ django_todo/todo_list/todo_list/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('TODO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('TODO_DEBUG_VALUE')
+DEBUG = (os.environ.get('TODO_DEBUG_VALUE')='True')
 
 ALLOWED_HOSTS = ['geeliztodo.herokuapp.com']
 
