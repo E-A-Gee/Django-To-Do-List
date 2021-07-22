97b7746fcf4a844e0e1ee6e752e9a00c59fbc4f1
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-07-21 20:34:32 -0400

    Added procfile

--- django_weather/weather_project/weather_app/views.py
+++ django_weather/weather_project/weather_app/views.py
@@ -9,26 +9,28 @@ def index(request):
         return render(request, 'weather_app/weather.html',{})
 
     else:
-        # Get user IP address
-        user_ip = request.META.get("REMOTE_ADDR")
+        # # Get user IP address
+        # user_ip = request.META.get("REMOTE_ADDR")
         
         # Get user longitude and latitude
-        ip_response = requests.get(f'http://ip-api.com/json/{user_ip}').json()
-        user_lat = ip_response['lat']
-        user_lon = ip_response['lon']
-        city = ip_response['city']
+        ip_response = requests.get(f'http://ip-api.com/json/108.14.13.170').json()
+        print(ip_response)
+        # user_lat = ip_response['lat']
+        # user_lon = ip_response['lon']
+        # city = ip_response['city']
         
-        #OpenWeatherAPI
-        open_weather_url= f'api.openweathermap.org/data/2.5/weather?lat={user_lat}&lon={user_lon}&appid=65d6a2854eae556a61d30eaa38bea1a6'
-        weather_response = requests.get(open_weather_url).json()
+        # #OpenWeatherAPI
+        # open_weather_url= f'api.openweathermap.org/data/2.5/weather?lat={user_lat}&lon={user_lon}&appid=65d6a2854eae556a61d30eaa38bea1a6'
+        # weather_response = requests.get(open_weather_url).json()
 
-        city_weather = {
-            'city': city,
-            'temperature': weather_response['main']['temp'],
-            'description':weather_response[0]['description'],
-            'icon': weather_response['weather'][0]['description']
-        }
+        # city_weather = {
+        #     'city': city,
+        #     'temperature': weather_response['main']['temp'],
+        #     'description':weather_response[0]['description'],
+        #     'icon': weather_response['weather'][0]['description']
+        # }
 
-        context = {'city_weather':city_weather}
+        # context = {'city_weather':city_weather}
 
-        return render(request, 'weather_app/weather.html', context)
+        return render(request, 'weather_app/weather.html')
+        # remember to enter in context
