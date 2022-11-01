88cbfc87be243abf0bd3f50f084f159ec5f4f216
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2022-10-31 22:32:31 -0400

    Add Railway to Allowed Hosts

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('TODO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['geeliztodo.herokuapp.com']
+ALLOWED_HOSTS = ['geeliz-todo-list.up.railway.app']
 
 
 # Application definition
